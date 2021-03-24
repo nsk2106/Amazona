06fb78a37b4b7e6469b5ffaf963ceb0b0c9349c0
nsk <nsk@Nehas-MacBook-Pro.local>
2021-03-23 21:24:27 -0400

    add files

---
+++ frontend/src/data.js
@@ -0,0 +1,68 @@
+const data = {
+    products : [
+        {
+            _id : '1',
+            name: 'Nike Slim Shirt',
+            category: 'shirt',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High Quality Product'
+           }, 
+           {
+            _id : '2',
+            name: 'Nike Slim Shirt',
+            category: 'shirt',
+            image: '/images/p2.jpg',
+            price: 120,
+            countInStock: 12,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High Quality Product'
+           }, 
+           {
+            _id : '3',
+            name: 'Adidas fit Shirt',
+            category: 'shirt',
+            image: '/images/p3.jpg',
+            price: 240,
+            countInStock: 0,
+            brand: 'Adidas',
+            rating: 3.5,
+            numReviews: 12,
+            description: 'High Quality Product'
+           }, 
+           {
+            _id : '4',
+            name: 'Puma pants',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 89,
+            countInStock: 20,
+            brand: 'Puma',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High Quality Product'
+           }, 
+           {
+            _id : '5',
+            name: 'Underarmour Slim pants',
+            category: 'Pants',
+            image: '/images/p5.jpg',
+            price: 230,
+            countInStock: 5,
+            brand: 'Underarmour',
+            rating: 4.5,
+            numReviews: 13,
+            description: 'High Quality Product'
+           },
+    ],
+    
+    };
+    
+    
+    export default data;
